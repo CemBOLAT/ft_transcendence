347072b4eb531d2c3805f419eee5704c7c5213cc
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-06-30 14:51:06 +0300

    done

--- project/home/views.py
+++ project/home/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.urls import reverse
 
-# Create your views here.
 
-def home(request, user):
-	return render(request, 'home/home.html', {'user': user})
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('home'))
+
+
+@login_required
+def home(request):
+    user = request.user
+    context = {
+        'username': user.username,
+        'email': user.email,
+    }
+    return render(request, 'home/home.html', context)
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('home'))
--- project/login/views.py
+++ project/login/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 
-# Create your views here.
 
-def login(request):
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			return redirect('/home/')
-		else:
-			return redirect('login')
-	return render(request, 'login/login.html')
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/home/')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('login')
+    return render(request, 'login/login.html')
 
 
 def signup(request):
@@ -27,11 +27,11 @@ def signup(request):
 
         if password != password2:
             messages.error(request, 'Passwords do not match.')
-            return render(request, 'login/signup.html')
+            return render(request, 'signup/signup.html')
 
         if User.objects.filter(username=username).exists():
             messages.error(request, 'Username already exists.')
-            return render(request, 'login/signup.html')
+            return render(request, 'signup/signup.html')
 
         user = User.objects.create_user(username=username, password=password)
         user.save()
@@ -39,4 +39,4 @@ def signup(request):
             request, 'Your account has been created successfully.')
         return redirect('login')
 
-    return render(request, 'login/signup.html')
+    return render(request, 'signup/signup.html')
